3d714306409338559b91d74b45a8d91582aad668
Breno Rev <silvabreno462@gmail.com>
2021-09-01 17:28:04 -0300

    Modificando o Enum de cargo

--- src/main/java/com/brenodev/enums/Cargo.java
+++ src/main/java/com/brenodev/enums/Cargo.java
@@ -1,10 +1,21 @@
 package com.brenodev.enums;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public enum Cargo {
 
-	ADMIN,
-	GERENTE,
-	CAIXA,
-	USUARIO
+	ADMIN("Administrador"),
+	GERENTE("Gerente"),
+	CAIXA("Caixa"),
+	REPOSITOR("Repositor");
+	
+	private String nome;
+	
+	private Cargo(String nome) {
+		this.nome = nome;
+	}
 	
 }
